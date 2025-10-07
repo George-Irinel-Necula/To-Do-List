14b8f6a542e69b1e00ef073727b9c839cce24ad4
George-Irinel-Necula <irinelnecula@yahoo.com>
2025-10-07 20:03:02 +0300

    Changed vite config build settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,17 @@ export default defineConfig({
     tailwindcss(),
   ],
   base: '/To-Do-List/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js', 
+        chunkFileNames: 'assets/[name]-[hash].js', 
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) return 'assets/index.css';
+          return 'assets/[name]-[hash][extname]'; 
+        },
+      },
+    },
+    cssCodeSplit: true, // ensures CSS is separate
+  },
 })

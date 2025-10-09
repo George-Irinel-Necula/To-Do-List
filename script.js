8046b3536fcfe824d8e44ba30626b6f341b46cbb
George Necula <irinelnecula@yahoo.com>
2025-10-09 14:35:52 +0300

    Added Automatic Backup in localStorage and Added Checkbox Save state on reload

--- script.js
+++ script.js
@@ -25,26 +25,36 @@ function taskCardToggle() {
   }
 }
 
-function updateIDs(){
-  for(let i=0;i<taskArray.length;i++){
-    taskArray[i].id=i
+function updateIDs() {
+  for (let i = 0; i < taskArray.length; i++) {
+    taskArray[i].id = i;
   }
 }
 
-function unRender(){
-  taskList.innerHTML=""
+function unRender() {
+  taskList.innerHTML = "";
 }
 
 function renderTasks(taskCard) {
-    taskList.insertAdjacentHTML(`beforeend`,`<div class="task-card w-full bg-base-100 min-h-28 rounded-md py-4 px-2 justify-between wrap-anywhere" id="${taskCard.id}" >
+  taskList.insertAdjacentHTML(
+    `beforeend`,
+    `<div class="task-card w-full bg-base-100 min-h-28 rounded-md py-4 px-2 justify-between wrap-anywhere" id="${
+      taskCard.id
+    }" >
         <div class="flex gap-2 items-top">
-            <div class="pl-4 pr-2"><input type="checkbox" class="checkbox checkbox-primary" id="defaultCheckbox1" ${taskCard.checked === true ? "checked" : ""}/>
+            <div class="pl-4 pr-2"><input type="checkbox" class="checkbox checkbox-primary" id="defaultCheckbox1" ${
+              taskCard.checkboxStatus === true ? "checked" : ""
+            }/>
             </div>
             <div class="flex flex-col gap-2 w-full">
-                <h1 class="task-title font-bold text-lg text-white">${taskCard.title}</h1>
+                <h1 class="task-title font-bold text-lg text-white">${
+                  taskCard.title
+                }</h1>
                 <p class="">${taskCard.description}</p>
                 <div class="info flex items-center gap-1 text-xs mt-2 ">
-                    <h1 class="min-w-fit info badge badge-primary badge-soft">Date created:${taskCard.date}</h1>
+                    <h1 class="min-w-fit info badge badge-primary badge-soft">Date created:${
+                      taskCard.date
+                    }</h1>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                         fill="currentColor" class="icon icon-tabler icons-tabler-filled size-3 icon-tabler-point hidden sm:flex">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -70,7 +80,8 @@ function renderTasks(taskCard) {
                 <button class="p-1 edit"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg></button>
             </div>
         </div>
-    </div>`)
+    </div>`
+  );
 }
 
 function addTask() {
@@ -85,12 +96,12 @@ function addTask() {
       id: taskArray.length,
       title: title.value,
       description: description.value,
-      checked: false,
+      checkboxStatus: false,
       date: getDate(),
     };
 
     taskArray.push(taskCard);
-    renderTasks(taskCard)
+    renderTasks(taskCard);
   }
 
   title.value = "";
@@ -98,19 +109,21 @@ function addTask() {
   taskCardToggle();
 
   console.log(taskArray);
+  saveTasks();
 }
-
 
 addTaskButton.addEventListener("click", addTask);
 
 function deleteTask(taskCard) {
   const taskId = parseInt(taskCard.id);
-  taskArray = taskArray.filter(task => {
-  if (task.id !== taskId) {
-    return true;
-  }})
+  taskArray = taskArray.filter((task) => {
+    if (task.id !== taskId) {
+      return true;
+    }
+  });
   console.log(taskArray);
   taskCardToggle();
+  saveTasks();
 }
 
 function clickDelete(e) {
@@ -118,14 +131,14 @@ function clickDelete(e) {
   if (!clickedDelete) return;
   let taskCard = e.target.closest(".task-card");
   if (clickedDelete) {
-    deleteTask(taskCard)
+    deleteTask(taskCard);
   }
-  unRender()
-  updateIDs()
-  for(let i=0;i<taskArray.length;i++){
-    renderTasks(taskArray[i])
+  unRender();
+  updateIDs();
+  for (let i = 0; i < taskArray.length; i++) {
+    renderTasks(taskArray[i]);
   }
-  taskCardToggle()
+  taskCardToggle();
   //Ar trebuii sa dau pop din array la elemetul sters
 }
 
@@ -138,14 +151,17 @@ function checkTask(e) {
   let status = taskCard.querySelector(".card-status");
   let title = taskCard.querySelector(".task-title");
   let paragraph = taskCard.querySelector("P");
+  let taskID = parseInt(taskCard.id);
   if (checkbox.checked) {
     status.classList.replace("badge-warning", "badge-success");
     checkbox.checked = true;
     status.textContent = "Status:Completed";
+    taskArray[taskID].checkboxStatus = true;
   } else {
     status.classList.replace("badge-success", "badge-warning");
     status.textContent = "Status:Pending";
     checkbox.checked = false;
+    taskArray[taskID].checkboxStatus = false;
   }
   title.classList.toggle("strike");
   paragraph.classList.toggle("strike");
@@ -164,17 +180,27 @@ function editTask(e) {
   title.value = titleCard.textContent;
   description.value = descriptionCard.textContent;
   if (edit) {
-    deleteTask(taskCard)
+    deleteTask(taskCard);
   }
-  unRender()
-  updateIDs()
-  for(let i=0;i<taskArray.length;i++){
-    renderTasks(taskArray[i])
+  unRender();
+  updateIDs();
+  for (let i = 0; i < taskArray.length; i++) {
+    renderTasks(taskArray[i]);
   }
-  taskCardToggle()
-  //Ar trebuii sa pun valorile in input ,
-  //sa iau indexul din array sa sterg obiectul din lista , dupa sa apelez functia de adaugare cu valorile editate din input field
+  taskCardToggle();
 }
 taskList.addEventListener("click", editTask);
 
+function saveTasks() {
+  localStorage.setItem("test", JSON.stringify(taskArray));
+}
 
+function loadTasks() {
+  taskArray = JSON.parse(localStorage.getItem("test"));
+  taskCardToggle();
+  taskArray.forEach((task) => {
+    renderTasks(task);
+  });
+  console.log(taskArray);
+}
+loadTasks();
